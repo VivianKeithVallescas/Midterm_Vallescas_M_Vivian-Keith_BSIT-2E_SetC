408f54b49a03ae7c3b8beb8760d430bff76230fa
VivianKeithVallescas <60762438+VivianKeithVallescas@users.noreply.github.com>
2020-03-22 23:36:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-function Generate(){
-    var a,b, result;
-
-    a=0;
-    b=1;
-    for(var i=1; i<100; i++)
-    {
-        document.write(result + "<br>");
-        result = a+b;
-        a=b;
-        b=result;
-    }
-}
 
 function Count(){
     for (var i = 1; i < 101; i++)
